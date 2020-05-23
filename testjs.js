e447f5f55bba7d2bea2a2280c66eca2a024086c9
f739 <65314363+f739@users.noreply.github.com>
2020-05-23 23:50:01 +0300

    Add files via upload

---
+++ testjs.js
@@ -0,0 +1,22 @@
+const type = value => {
+    let typeis = typeof value;
+    if (typeis === "number") {
+    }else if (typeis === "string") {
+    }else if (typeis === "boolean"){
+    }else if (typeis === "object"){
+    }else if (typeis === "undefined"){
+    }else if (typeis === "function"){
+    }else if (typeis === "bigint"){
+    }else if (typeis === "symbol"){};
+    console.log(`this is ${value} a ${typeis}`);
+
+}; 
+type('');
+
+
+
+let b = arg1 => {
+    let arg2 = arg1 
+    return (arg2)
+}
+console.log(true)
